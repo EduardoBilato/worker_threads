c15239eae081fb6aed70f66a606ef46989f73746
EduardoBilato <eduardobilato@gmail.com>
2025-02-01 14:05:29 -0300

    refactor: improve log and request more size on queue

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const Pool = require('worker-thread-pool');
 
 const pool = new Pool({
     path: __dirname + '/worker.js',
-    size: 5,
-    maxQueueSize: 50,
+    size: 150,
+    maxQueueSize: 1000,
 });
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i <= 1000; i++) {
 
     const name = i.toString();
 
     pool.run({ name })
         .then((result) => {
-            console.log('result', result);
+            console.info(result);
         })
         .catch((err) => {
             console.log('err', err);
--- worker.js
+++ worker.js
@@ -2,18 +2,18 @@ const { parentPort } = require('worker_threads');
 
 parentPort.on('message', (message) => {
 
-    console.log('hello ' + message.name);
+    // console.log('hello ' + message.name);
 
     const time = getRandomTime();
 
     setTimeout(() => {
-        message.port.postMessage({ message: message.name, waitTime: time , done: 'ok' });
+        message.port.postMessage({ message: message.name.toString().padStart(4, '0'), waitTime: time, done: 'ok' });
         message.port.close();
     }, time);
 
 });
 
 function getRandomTime() {
-    const fator = Math.floor(Math.random() * 5) + 1;
-    return fator * 500;
+    const fator = Math.floor(Math.random() * 5000) + 1;
+    return fator;
 }
